9d15a0598971f4d7874ff53ba1b9ca90dc19c73f
levz86 <levina.ramkumar@gmail.com>
2023-08-09 19:03:40 +0200

    first commit

---
+++ personal/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class PersonalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'personal'
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
